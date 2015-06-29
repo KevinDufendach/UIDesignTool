463ab43e29d99823d0e63463236d36174e4dcaa7
Kevin Dufendach <krd.public+github@gmail.com>
2015-06-29 15:11:34 -0500

    add files

---
+++ war/js/app.js
@@ -0,0 +1,47 @@
+(function(){
+	var app = angular.module('library', [ 'library-products' ]);
+
+	app.controller("LibraryController", ['$http', function($http){ // this is a controller
+		//this.product = gem; // product is a property of controller
+		
+		var library = this;
+		library.products = [ ];
+		
+		// $http.post('/path/to/resource.json', {param: 'value'});
+		// $http.delete('/path/to/resource.json');
+		
+		// $http({ method: 'OPTIONS', url: '/path/to/resource.json'}); // PATCH TRACE
+		
+		$http.get('products.json').success(function(data) {
+			library.products = data;
+		}); // product is a property of controller
+		
+	} ]);
+	
+	//var gems = ;
+	
+})();
+
+// (function(){
+	// var app = angular.module('store', [ 'store-products' ]);
+
+	// app.controller("StoreController", ['$http', function($http){ // this is a controller
+		// // this.product = gem; // product is a property of controller
+		
+		// var store = this;
+		// store.products = [ ];
+		
+		// // $http.post('/path/to/resource.json', {param: 'value'});
+		// // $http.delete('/path/to/resource.json');
+		
+		// // $http({ method: 'OPTIONS', url: '/path/to/resource.json'}); // PATCH TRACE
+		
+		// $http.get('products.json').success(function(data) {
+			// store.products = data;
+		// }); // product is a property of controller
+		
+	// } ]);
+	
+	// // var gems = ;
+	
+// })();
